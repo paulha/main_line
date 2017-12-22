a7d9633d36e1db99f854c9b3d21e278b48f77b39
Paul Hanchett <pfhanchx@intel.com>
2017-12-21 16:14:06 -0800

    Use local credentials, parse what comes back from DNG as XML
    Considering how long I've been at this, very encouraging progress!

--- access_dng.py
+++ access_dng.py
@@ -1,11 +1,34 @@
 import requests
 
+# -- Support
+import sys
+from os.path import pathsep, dirname, realpath
+from utility_funcs.search import get_server_info
+import utility_funcs.logger_yaml as log
+
+from lxml import etree
+
+# -- Set up logger...
+LOG_CONFIG_FILE = 'logging.yaml'+pathsep+dirname(realpath(sys.argv[0]))+'/logging.yaml'
+log.setup_logging("logging.yaml", override={'handlers': {'info_file_handler': {'filename': 'audit_jama_jira_matchup.log'}}})
+log.logger.setLevel(log.logging.getLevelName("INFO"))
+log.logger.disabled = False
+
+JAZZ_CONFIG_FILE = dirname(realpath(sys.argv[0])) + '/config.yaml' + pathsep + '~/.jazz/config.yaml'
+
 #----------------------------------------------------
 #       Fill in valid username and password!
 #----------------------------------------------------
-username = "pfhanchx"
-password = "Wouldn't you like to know?"
+try:
+    jazz_config = get_server_info("dng", JAZZ_CONFIG_FILE)  # possible FileNotFoundError
+except FileNotFoundError as f:
+    self.log.fatal("Can't open JAZZ authentication configuration file: %s" % f)
+    raise FileNotFoundError("Can't find Jazz configuration file", JAZZ_CONFIG_FILE)
 
+log.logger.info(f"Using JAMA server instance {jazz_config['host']}{jazz_config['instance']}")
+
+
+"""
 if False:
     # -- OTC values
     authenticate = 'https://rtc.intel.com/rrc/auth/j_security_check'
@@ -18,6 +41,7 @@ else:
     get_catalog = 'https://rtc.intel.com/dng0001001/oslc_rm/catalog'
     # _zQHY0a_4EeekDP1y4xXYPQ is the project ID of "SSG-OTC Product Management - DNG"
     get_project_services = 'https://rtc.intel.com/dng0001001/oslc_rm/_zQHY0a_4EeekDP1y4xXYPQ/services.xml'
+"""
 
 with requests.Session() as sess:
     # -- Using a session seems to work better than isolated gets and puts.
@@ -28,32 +52,85 @@ with requests.Session() as sess:
     #    which results in an error message: 'Invalid path to authentication servlet.' None the
     #    less, the login apparently succeeds.
     #
-    login_response = sess.post(authenticate,
+    login_response = sess.post(f"{jazz_config['host']}{jazz_config['instance']}/auth/j_security_check",
                                headers={"Content-Type": "application/x-www-form-urlencoded"},
-                               data=f"j_username={username}&j_password={password}",
+                               data=f"j_username={jazz_config['username']}&j_password={jazz_config['password']}",
                                verify=False)
     print(f"Login response: {login_response.status_code}")
     print(f"Login cookies: {login_response.cookies}")
-    print(f"{login_response.text}\n=========================")
+    # print(f"{login_response.text}\n=========================")
 
     # -- This line works, even if login above fails...
-    root_services_response = sess.get(get_root_services,
-                                      headers={'OSLC-Core-Version': '2.0', 'Accept': 'application/rdf+xml'})
+    root_services_response = sess.get(f"{jazz_config['host']}{jazz_config['instance']}/rootservices",
+                                      headers={'OSLC-Core-Version': '2.0', 'Accept': 'application/rdf+xml'},
+                                      stream=True, verify=False)
     print(f"Root Services response: {root_services_response.status_code}")
     print(f"Root Services cookies: {root_services_response.cookies}")
-    print(f"{root_services_response.text}\n=========================")
+    # Note: Printing this here, causes the parse below to fail (because the stream is already read...?)
+    # print(f"{root_services_response.text}\n=========================")
 
     # -- This line only gives a result if the login succeeded, but the output is MUCH shorter than I expect.
-    catalog_response = sess.get(get_catalog,
-                                headers={'OSLC-Core-Version': '2.0', 'Accept': 'application/rdf+xml'})
+    olsc_namespaces = {
+        'dc': 'http://purl.org/dc/terms/',
+        # 'jfs_proc' : 'http://jazz.net/xmlns/prod/jazz/process/1.0/',
+        # 'oslc_disc': 'http://open-services.net/xmlns/discovery/1.0/',
+        'oslc_rm': "http://open-services.net/xmlns/rm/1.0/",
+        'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+        'oslc': 'http://open-services.net/ns/core#',
+        'dcterms': "http://purl.org/dc/terms/",
+        'jp': "http://jazz.net/xmlns/prod/jazz/process/1.0/"
+    }
+    root_services_response.raw.decode_content = True
+    pa_root_services = etree.parse(root_services_response.raw)
+    catalog_url = pa_root_services.xpath('//oslc_rm:rmServiceProviders/@rdf:resource', namespaces=olsc_namespaces)
+    log.logger.info(f"Catalog URL is {catalog_url}")
+
+    catalog_response = sess.get(catalog_url[0],
+                                headers={'OSLC-Core-Version': '2.0', 'Accept': 'application/rdf+xml'},
+                                stream=True, verify=False)
     print(f"Catalog response: {catalog_response.status_code}")
     print(f"Catalog cookies: {catalog_response.cookies}")
-    print(f"{catalog_response.text}\n=========================")
+
+    catalog_response.raw.decode_content = True
+    pa_project_services = etree.parse(catalog_response.raw)
+    log.logger.info(f"{etree.tostring(pa_project_services, pretty_print=True)}\n=========================")
+    projects = pa_project_services.xpath('//oslc:ServiceProvider', namespaces=olsc_namespaces)
+    log.logger.info(f"Projects is {projects}")
+
+    catalog_services = [
+        {
+            'services': project.xpath('//oslc:ServiceProvider/@rdf:about', namespaces=olsc_namespaces)[0],
+            'title': project.xpath('dcterms:title/text()', namespaces=olsc_namespaces)[0],
+            'registry': project.xpath('jp:consumerRegistry/@rdf:resource', namespaces=olsc_namespaces)[0],
+            'details': project.xpath('oslc:details//@rdf:resource', namespaces=olsc_namespaces)[0],
+        }
+        for project in projects
+    ]
+    log.logger.info(f"{catalog_services}")
 
     # -- This line gets services of project "SSG-OTC Product Management - DNG"
-    project_services_response = sess.get(get_project_services,
-                                         headers={'OSLC-Core-Version': '2.0', 'Accept': 'application/rdf+xml'})
+
+    project_services_response = sess.get(catalog_services[1]['services'],
+                                         headers={'OSLC-Core-Version': '2.0', 'Accept': 'application/rdf+xml'},
+                                         stream=True, verify=False)
     print(f"Project Services response: {project_services_response.status_code}")
     print(f"Project Services cookies: {project_services_response.cookies}")
-    print(f"{project_services_response.text}\n=========================")
+    # print(f"{project_services_response.text}\n=========================")
+
+    project_services_response.raw.decode_content = True
+    pa_services = etree.parse(project_services_response.raw)
+    log.logger.info(f"{etree.tostring(pa_services, pretty_print=True)}\n=========================")
+    services = pa_services.xpath('//oslc:service', namespaces=olsc_namespaces)
+    log.logger.info(f"Projects is {projects}")
+
+    services = [
+        {
+            #'services': project.xpath('//oslc:ServiceProvider/@rdf:about', namespaces=olsc_namespaces)[0],
+            'title': service.xpath('./oslc:Service//dcterms:title/text()', namespaces=olsc_namespaces),
+            #'registry': project.xpath('jp:consumerRegistry/@rdf:resource', namespaces=olsc_namespaces)[0],
+            #'details': project.xpath('oslc:details//@rdf:resource', namespaces=olsc_namespaces)[0],
+        }
+        for service in services
+    ]
+    log.logger.info(f"{services}")
 
