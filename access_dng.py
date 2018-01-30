d73a1aa959da13971fe2b55799198aa1c909a860
Paul Hanchett <pfhanchx@intel.com>
2018-01-29 17:50:10 -0800

    Add new functionality from Rational's Java code workshop

--- access_dng.py
+++ access_dng.py
@@ -176,6 +176,14 @@ class Jazz(requests.Session):
 
                     project_info['services'][service_info['title']] = service_info
 
+    def get_service_provider(self):
+
+        project_xml_tree = self._get_xml(self.RootServices['catalogs'][0]['url'], "Project Catalog")
+        service_provider = project_xml_tree.xpath("//oslc:ServiceProvider[dcterms:title='" \
+                                                  + self.jazz_config.['project'] + "']/./@rdf:about",
+                                                  namespaces=self.namespace)
+        return service_provider[0]
+
     def query(self, oslc_prefix=None, oslc_select=None, oslc_where=None):
         query_section = self.RootServices['catalogs'][0]['projects'][self.jazz_config['project']]['services']['Query Capability']
         query = query_section['queryBase']
@@ -191,53 +199,158 @@ class Jazz(requests.Session):
         where = "&"+urlencode({'oslc.where': oslc_where}) if oslc_where is not None else ""
         query_text = f"{query}{prefix}{select}{where}"
         query_root = self._get_xml(query_text, "Query")
-        query_result = {'query_text': query_text, 'query_result': etree.tostring(query_root)}
+        query_result = {'query_text': query_text, 'query_root': query_root, 'query_result': etree.tostring(query_root)}
         self._add_from_xml(query_result, query_root, 'result', './oslc:ResponseInfo/dcterms:title', func=Jazz._get_text)
         self._add_from_xml(query_result, query_root, 'about', './rdf:Description/@rdf:about', func=Jazz._get_first)
         self._add_from_xml(query_result, query_root, 'RequirementCollections', '//oslc_rm:RequirementCollection/@rdf:about')
         self._add_from_xml(query_result, query_root, 'Requirements', '//oslc_rm:Requirement/@rdf:about')
         return query_result
 
-    def read(self, resource_url):
-        root_element = self._get_xml(resource_url, "item", mode="a+")
+    def _get_resources(self, resource_list, section_tag='resources'):
+        return [self._get_xml(resource_url, section_tag, mode="a+") for resource_url in resource_list]
+
+
+    def read(self, resource_url, section_tag='item'):
+        root_element = self._get_xml(resource_url, section_tag, mode="a+")
         this_item = {'Root': root_element, 'resource_url': resource_url}
         self._add_from_xml(this_item, root_element, 'about', './rdf:Description/@rdf:about')
         self._add_from_xml(this_item, root_element, 'modified', './rdf:Description/dcterms:modified', func=Jazz._get_text)
         self._add_from_xml(this_item, root_element, 'description', './rdf:Description/dcterms:description', func=Jazz._get_text)
         self._add_from_xml(this_item, root_element, 'creator', './rdf:Description/dcterms:creator/@rdf:about')
         self._add_from_xml(this_item, root_element, 'created', './rdf:Description/dcterms:created', func=Jazz._get_text)
-        self._add_from_xml(this_item, root_element, 'ServiceProvider', './rdf:Description/oslc:serviceProvider/@rdf:resource')
+        self._add_from_xml(this_item, root_element, 'ServiceProvider', './rdf:Description/oslc:serviceProvider/@rdf:resource', func=self._get_resources)
         self._add_from_xml(this_item, root_element, 'access_control', './rdf:Description/acp:accessContro/@rdf:resource')
         self._add_from_xml(this_item, root_element, 'primary_text', './rdf:Description/jazz_rm:primaryText')
         self._add_from_xml(this_item, root_element, 'type', './rdf:Description/rdf:type/@rdf:resource')
-        self._add_from_xml(this_item, root_element, 'contributor', './rdf:Description/dcterms:contributor/@rdf:resource')
-        self._add_from_xml(this_item, root_element, 'projectArea', './rdf:Description/process:projectArea/@rdf:resource')
-        self._add_from_xml(this_item, root_element, 'component', './rdf:Description/oslc_config:component/@rdf:resource')
+        self._add_from_xml(this_item, root_element, 'contributor', './rdf:Description/dcterms:contributor/@rdf:resource', func=self._get_resources)
+        self._add_from_xml(this_item, root_element, 'projectArea', './rdf:Description/process:projectArea/@rdf:resource', func=self._get_resources)
+        self._add_from_xml(this_item, root_element, 'component', './rdf:Description/oslc_config:component/@rdf:resource', func=self._get_resources)
         self._add_from_xml(this_item, root_element, 'identifier', './rdf:Description/dcterms:identifier', func=Jazz._get_text)
         self._add_from_xml(this_item, root_element, 'title', './rdf:Description/dcterms:title', func=Jazz._get_text)
-        self._add_from_xml(this_item, root_element, 'parent', './rdf:Description/nav:parent/@rdf:resource')
+        self._add_from_xml(this_item, root_element, 'parent', './rdf:Description/nav:parent/@rdf:resource', func=self._get_resources)
         self._add_from_xml(this_item, root_element, 'instanceShape', './rdf:Description/oslc:instanceShape/@rdf:resource')
         # -- Entry below should be for a collection...
         self._add_from_xml(this_item, root_element, 'uses', '//oslc_rm:uses/@rdf:resource')
         # todo: This looks like a special form and should be handled as such...
-        self._add_from_xml(this_item, root_element, 'rm_property:_5LMQKK_4EeekDP1y4xXYPQ', './rdf:Description/rm_property:_5LMQKK_4EeekDP1y4xXYPQ/@rdf:resource')
-        self._add_from_xml(this_item, root_element, 'rm_property:_4G6Ioa_4EeekDP1y4xXYPQ', './rdf:Description/rm_property:_4G6Ioa_4EeekDP1y4xXYPQ/@rdf:resource')
+        self._add_from_xml(this_item, root_element, 'rm_property', './rdf:Description/rm_property:*/@rdf:resource', func=self._get_resources)
+        # self._add_from_xml(this_item, root_element, 'rm_property:_4G6Ioa_4EeekDP1y4xXYPQ', './rdf:Description/rm_property:_4G6Ioa_4EeekDP1y4xXYPQ/@rdf:resource', func=self._get_resources)
         self.logger.debug(this_item)
         return this_item
 
+    def discover_root_folder():
+        query = new HttpGet(self.get_service_provider());
+        query.addHeader("Accept", "application/rdf+xml");
+        query.addHeader("OSLC-Core-Version", "2.0");
+
+        # -- Access to the Service Providers catalog
+        response = HttpUtils.sendGetForSecureDocument( server, query, login, password, httpclient, JTS_Server);
+
+        if (response.getStatusLine().getStatusCode() != 200):
+            response.getEntity().consumeContent();
+            throw new HttpResponseException(response.getStatusLine().getStatusCode(), response.getStatusLine().getReasonPhrase());
+
+        // Define the XPath evaluation environment
+        xpath = getXpathNamespace();
+
+        queryCapability = "//oslc:QueryCapability[dcterms:title=\"Folder Query Capability\"]/oslc:queryBase/@rdf:resource";
+
+        # -- Parse the response body to retrieve the catalog URI
+        InputSource source = new InputSource(response.getEntity().getContent());
+        Node attribute = (Node) (xpath.evaluate(queryCapability, source, XPathConstants.NODE));
+        String queryCapabilityUrl = attribute.getTextContent();
+        response.getEntity().consumeContent();
+
+        query = new HttpGet(queryCapabilityUrl);
+        query.addHeader("Accept", "application/rdf+xml");
+        query.addHeader("OSLC-Core-Version", "2.0");
+
+        # --Access to the Service Providers catalog
+        response = HttpUtils.sendGetForSecureDocument(
+        server, query, login, password, httpclient, JTS_Server);
+
+        if (response.getStatusLine().getStatusCode() != 200):
+            response.getEntity().consumeContent();
+            throw new HttpResponseException(response.getStatusLine().getStatusCode(), response.getStatusLine().getReasonPhrase());
+
+        queryCapability = "//nav:folder[dcterms:title=\"root\"]/@rdf:about";
+
+        # --Parse the response body to retrieve the catalog URI
+        xpath = getXpathNamespace();
+        source = new InputSource(response.getEntity().getContent());
+        attribute = (Node) (xpath.evaluate(queryCapability, source, XPathConstants.NODE));
+        String rootFolder = attribute.getTextContent();
+        response.getEntity().consumeContent();
+
+        return rootFolder;
+
+
+    def createFolder(self, folderName: str, parentfolder: str=None, project: str=None) -> str:
+        serviceProviderUrl = self.get_service_provider()
+
+        # Get the Project ID String
+        projectID = serviceProviderUrl.substring(0, serviceProviderUrl.lastIndexOf("/"));
+        projectID = projectID.substring(projectID.lastIndexOf("/") + 1);
+
+        if (folderName == null):
+            folderName = "OSLC Created";
+
+        if (parentfolder == null):
+            # -- lets create it on the root \
+            # -- parentfolder = this.server + "/folders/" + projectID;
+            # -- https: // jazz.net / jazz03 / web / projects / Requirements % 20 Management # action=com.ibm.team.workitem.viewWorkItem&id=114458
+            parentfolder = discoverRootFolder(serviceProviderUrl);
+
+        # -- 6.0 update - changed to using rm server for targetProject URL, not the JTS server
+        # -- Create the URL Creation factory
+        String targetProject = "?projectURL=" + this.server + "/process/project-areas/" + projectID;
+        String folderCreationFactory = this.server + "/folders" + targetProject;
+
+        xml = f"""
+            <rdf:RDF
+                    xmlns:nav="http://jazz.net/ns/rm/navigation#" 
+                    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                    xmlns:oslc_rm="http://open-services.net/ns/rm#"
+                    xmlns:oslc="http://open-services.net/ns/core#"
+                    xmlns:rmTypes="http://www.ibm.com/xmlns/rdm/types/"
+                    xmlns:dcterms="http://purl.org/dc/terms/"
+                    xmlns:rm="http://jazz.net/ns/rm#">
+                <rdf:Description rdf:nodeID="A0">
+                    <rdf:type rdf:resource="http://jazz.net/ns/rm/navigation#folder"/>
+                    <dcterms:title rdf:datatype="http://www.w3.org/2001/XMLSchema#string">{folderName}</dcterms:title>
+                    <nav:parent rdf:resource="{parentfolder}"/>
+                </rdf:Description>
+            </rdf:RDF>
+        """;
+
+        // Post to the Requirement Factory
+        
+        HttpPost post = new HttpPost(folderCreationFactory);
+        post.addHeader("Accept", "application/rdf+xml");
+        post.addHeader("Content-Type", "application/rdf+xml");
+        post.addHeader("OSLC-Core-Version", "2.0");
+        post.setEntity(new StringEntity(xml, HTTP.UTF_8));
+        // -- FIXME: Throws a 302 error 
+        HttpResponse sendPostForSecureDocument = HttpUtils.sendPostForSecureDocument(server, post, login, password, httpclient);
+        
+        Header folderLocation = sendPostForSecureDocument.getFirstHeader("Location");
+        sendPostForSecureDocument.getEntity().consumeContent();
+        return folderLocation.getValue();
+
 
 def main():
-    j = Jazz(server_alias="dng", config_path=JAZZ_CONFIG_PATH)
+    j = Jazz(server_alias="sandbox", config_path=JAZZ_CONFIG_PATH)
     query_result = j.query(# oslc_prefix='rdf=<http://www.w3.org/1999/02/22-rdf-syntax-ns#>,calm=<http://jazz.net/xmlns/prod/jazz/calm/1.0>,rm=<http://www.ibm.com/xmlns/rdm/rdf/>,oslc=<http://open-services.net/ns/core#>,jp10=<http://jazz.net/xmlns/prod/jazz/process/1.0/>,oslc_config=<http://open-services.net/ns/config#>,dcterms=<http://purl.org/dc/terms/>',
                            oslc_prefix='dcterms=<http://purl.org/dc/terms/>',
-                           oslc_select='dcterms:identifier',
-                           oslc_where='dcterms:identifier=67120'
+                           oslc_select='*',         # 'dcterms:identifier',
+                           oslc_where='dcterms:identifier=67383'
                            )
+    
+    # import xmltodict
 
 
     if 'Requirements' in query_result:
         # Note: even if the select above limits the returned fields, the read below will read the entire item!
-        requirements = {item['identifier']: item for item in [j.read(member) for member in query_result['Requirements'][:2]]}
+        requirements = {item['identifier']: item for item in [member for member in query_result['Requirements'][:2]]}
         log.logger.info("\n\n%d items in requirements", len(requirements))
         p = pd.DataFrame(requirements)
         log.logger.info(p)
--- access_dng_test.py
+++ access_dng_test.py
@@ -2,126 +2,173 @@ import unittest
 
 import access_dng as ad
 import urllib3
+import lxml as etree
 
-TEST_RECORD = 50161
+# -- DNG/One Android/Programs/Test Integration
+TEST_RECORD_1 = 67383               # 50161
+TEST_RECORD_2 = 67382               # 50162
 SELECT_ALL = '*'
 SELECT_ONE = "dcterms:identifier"
 SELECT_TWO = "dcterms:title,dcterms:description"
 WHERE_ALL = "*"
-WHERE_ONE = f"dcterms:identifier={TEST_RECORD}"
+WHERE_ONE = f"dcterms:identifier={TEST_RECORD_1}"
+WHERE_TWO = f"dcterms:identifier in [{TEST_RECORD_1},{TEST_RECORD_2}]"
 
-class QueryTestCase(unittest.TestCase):
+PROJECT = 'Open Requirements Sandbox'
 
+# -- Login
+urllib3.disable_warnings()
+jazz = ad.Jazz(server_alias="sandbox", config_path=ad.JAZZ_CONFIG_PATH)
+
+TEST_CREATE = True
+TEST_QUERY = False
+
+
+class Jazz_Test(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         urllib3.disable_warnings()
-        cls.jazz = ad.Jazz(server_alias="dng", config_path=ad.JAZZ_CONFIG_PATH)
+        super().setUpClass()
+        cls.jazz = jazz
 
     @classmethod
     def tearDownClass(cls):
-        cls.jazz.close()
+        super().tearDownClass()
 
-    def test_query_prefix_00(self):
-        """Test with a single prefix"""
+
+
+if TEST_QUERY:
+    class TestQueryPrefix(Jazz_Test):
+        def test_query_prefix_00(self):
+            """Test with a single prefix"""
+            query_result = self.jazz.query(
+                oslc_prefix='dcterms=<http://purl.org/dc/terms/>',
+                oslc_select=SELECT_TWO
+            )
+            # todo: Check that the correct fields are returned
+            self.assertIn('result', query_result, "Should have a result")
+            self.assertGreater(len(query_result['Requirements']), 10, "Should be many results")
+            self.assertIn('RequirementCollections', query_result, "Requirements collections should be returned")
+            self.assertGreater(len(query_result['RequirementCollections']), 2, "Should be several collections")
+
+        def test_query_prefix_01(self):
+            """Test with two prefixes"""
+            query_result = self.jazz.query(
+                oslc_prefix='dcterms=<http://purl.org/dc/terms/>,oslc_rm=<http://open-services.net/ns/rm%23>',
+                oslc_select=SELECT_TWO
+            )
+            # todo: Check that the correct fields are returned
+            self.assertIn('result', query_result, "Should have a result")
+            self.assertGreater(len(query_result['Requirements']), 10, "Should be many results")
+            self.assertIn('RequirementCollections', query_result, "Requirements collections should be returned")
+            self.assertGreater(len(query_result['RequirementCollections']), 2, "Should be several collections")
+
+        def test_query_prefix_02(self):
+            """Test with many prefixes"""
+            query_result = self.jazz.query(
+                oslc_prefix='rdf=<http://www.w3.org/1999/02/22-rdf-syntax-ns#>,calm=<http://jazz.net/xmlns/prod/jazz/calm/1.0>,rm=<http://www.ibm.com/xmlns/rdm/rdf/>,oslc=<http://open-services.net/ns/core#>,jp10=<http://jazz.net/xmlns/prod/jazz/process/1.0/>,oslc_config=<http://open-services.net/ns/config#>,dcterms=<http://purl.org/dc/terms/>',
+                oslc_select=SELECT_TWO
+            )
+            # todo: Check that the correct fields are returned
+            self.assertIn('result', query_result, "Should have a result")
+            self.assertGreater(len(query_result['Requirements']), 10, "Should be many results")
+            self.assertIn('RequirementCollections', query_result, "Requirements collections should be returned")
+            self.assertGreater(len(query_result['RequirementCollections']), 2, "Should be several collections")
+
+        def test_query_prefix_03(self):
+            """Test with (No) prefixes, defaults to namespaces list"""
+            # fixme: This certainly won't work if the one above doesn't...
+            self.jazz.add_namespace('dcterms', 'http://purl.org/dc/terms/')
+            query_result = self.jazz.query(
+                oslc_select=SELECT_TWO
+            )
+            # todo: Check that the correct fields are returned
+            self.assertIn('result', query_result, "Should have a result")
+            self.assertGreater(len(query_result['Requirements']), 10, "Should be many results")
+            self.assertIn('RequirementCollections', query_result, "Requirements collections should be returned")
+            self.assertGreater(len(query_result['RequirementCollections']), 2, "Should be several collections")
+
+
+    class TestQuerySelect(Jazz_Test):
+        def test_query_select_00(self):
+            """Locate dcterms:identifier=67120, no selected fields (should return all)"""
+            query_result = self.jazz.query(
+                oslc_prefix='dcterms=<http://purl.org/dc/terms/>',
+                oslc_where=WHERE_ONE
+            )
+            # todo: Check that the correct fields are returned
+            self.assertEqual("Query Results: 1", query_result['result'], "Should return only one record.")
+            self.assertEqual(1, len(query_result['Requirements']), "Should be only one result")
+            self.assertNotIn('RequirementCollections', query_result, "No requirements collections should be returned")
+
+        def test_query_select_01(self):
+            """Locate all (no where), return only dcterms:identifier"""
+            query_result = self.jazz.query(
+                oslc_prefix='dcterms=<http://purl.org/dc/terms/>',
+                oslc_select=SELECT_ONE,
+            )
+            # -- todo: This test should be improved to know that it's really returning (only) the field I asked for...
+            self.assertGreater(len(query_result['Requirements']), 3, "Should be many results")
+            # self.assertNotIn('RequirementCollections', query_result, "No requirements collections should be returned")
+
+        def test_query_select_02(self):
+            """Locate dcterms:identifier=67120, select all fields ('*')"""
+            query_result = self.jazz.query(
+                oslc_prefix='dcterms=<http://purl.org/dc/terms/>',
+                oslc_select=SELECT_ALL,
+                oslc_where=WHERE_ONE
+            )
+            # todo: Check that the correct fields are returned
+            self.assertEqual("Query Results: 1", query_result['result'], "Should return only one record.")
+            self.assertEqual(1, len(query_result['Requirements']), "Should be only one result")
+            self.assertNotIn('RequirementCollections', query_result, "No requirements collections should be returned")
+
+
+    class TestQueryWhere(Jazz_Test):
+        def test_query_where_03(self):
+            """Locate dcterms:identifier in [{TEST_RECORD_1},{TEST_RECORD_2}], select all fields ('*')"""
+            query_result = self.jazz.query(
+                oslc_prefix='dcterms=<http://purl.org/dc/terms/>',
+                oslc_select='*',
+                oslc_where=WHERE_TWO
+            )
+            # todo: Check that the correct fields are returned
+            self.assertEqual("Query Results: 2", query_result['result'], "Should return two records.")
+            self.assertEqual(2, len(query_result['Requirements']), "Should be two result")
+            self.assertNotIn('RequirementCollections', query_result, "No requirements collections should be returned")
+
+            result = self.jazz.read(query_result['Requirements'][0])
+            pass
+
+if TEST_CREATE:
+    class TestCreateFolder(Jazz_Test):
+        def test_create_folder(self):
+            self.assertEqual(self.jazz.get_service_provider(PROJECT),
+                             "https://rtc-sbox.intel.com/rrc/oslc_rm/_xf5p4XNnEeecjP8b5e9Miw/services.xml",
+                             "get service provider URL")
+
+
+'''
+""" This is not even close to how it needs to work, I'm afraid...
+class TestUpdate(Jazz_Test):
+    def test_update_00(self):
+        """Locate dcterms:identifier={TEST_RECORD_1}, select all fields ('*')"""
         query_result = self.jazz.query(
             oslc_prefix='dcterms=<http://purl.org/dc/terms/>',
-            oslc_select=SELECT_TWO
-        )
-        # todo: Check that the correct fields are returned
-        self.assertIn('result', query_result, "Should have a result")
-        self.assertGreater(len(query_result['Requirements']), 10, "Should be many results")
-        self.assertIn('RequirementCollections', query_result, "Requirements collections should be returned")
-        self.assertGreater(len(query_result['RequirementCollections']), 2, "Should be several collections")
-
-    def test_query_prefix_01(self):
-        """Test with two prefixes"""
-        query_result = self.jazz.query(
-            oslc_prefix='dcterms=<http://purl.org/dc/terms/>,oslc_rm=<http://open-services.net/ns/rm%23>',
-            oslc_select=SELECT_TWO
-        )
-        # todo: Check that the correct fields are returned
-        self.assertIn('result', query_result, "Should have a result")
-        self.assertGreater(len(query_result['Requirements']), 10, "Should be many results")
-        self.assertIn('RequirementCollections', query_result, "Requirements collections should be returned")
-        self.assertGreater(len(query_result['RequirementCollections']), 2, "Should be several collections")
-
-    def test_query_prefix_02(self):
-        """Test with many prefixes"""
-        query_result = self.jazz.query(
-            oslc_prefix='rdf=<http://www.w3.org/1999/02/22-rdf-syntax-ns#>,calm=<http://jazz.net/xmlns/prod/jazz/calm/1.0>,rm=<http://www.ibm.com/xmlns/rdm/rdf/>,oslc=<http://open-services.net/ns/core#>,jp10=<http://jazz.net/xmlns/prod/jazz/process/1.0/>,oslc_config=<http://open-services.net/ns/config#>,dcterms=<http://purl.org/dc/terms/>',
-            oslc_select=SELECT_TWO
-        )
-        # todo: Check that the correct fields are returned
-        self.assertIn('result', query_result, "Should have a result")
-        self.assertGreater(len(query_result['Requirements']), 10, "Should be many results")
-        self.assertIn('RequirementCollections', query_result, "Requirements collections should be returned")
-        self.assertGreater(len(query_result['RequirementCollections']), 2, "Should be several collections")
-
-    def test_query_prefix_03(self):
-        """Test with (No) prefixes, defaults to namespaces list"""
-        # fixme: This certainly won't work if the one above doesn't...
-        self.jazz.add_namespace('dcterms', 'http://purl.org/dc/terms/')
-        query_result = self.jazz.query(
-            oslc_select=SELECT_TWO
-        )
-        # todo: Check that the correct fields are returned
-        self.assertIn('result', query_result, "Should have a result")
-        self.assertGreater(len(query_result['Requirements']), 10, "Should be many results")
-        self.assertIn('RequirementCollections', query_result, "Requirements collections should be returned")
-        self.assertGreater(len(query_result['RequirementCollections']), 2, "Should be several collections")
-
-    def test_query_select_00(self):
-        """Locate dcterms:identifier=67120, no selected fields (should return all)"""
-        query_result = self.jazz.query(
-            oslc_prefix='dcterms=<http://purl.org/dc/terms/>',
-            oslc_where=WHERE_ONE
-        )
-        # todo: Check that the correct fields are returned
-        self.assertEqual("Query Results: 1", query_result['result'], "Should return only one record.")
-        self.assertEqual(1, len(query_result['Requirements']), "Should be only one result")
-        self.assertNotIn('RequirementCollections', query_result, "No requirements collections should be returned")
-
-    def test_query_select_01(self):
-        """Locate all (no where), return only dcterms:identifier"""
-        query_result = self.jazz.query(
-            # oslc_prefix='rdf=<http://www.w3.org/1999/02/22-rdf-syntax-ns#>,calm=<http://jazz.net/xmlns/prod/jazz/calm/1.0>,rm=<http://www.ibm.com/xmlns/rdm/rdf/>,oslc=<http://open-services.net/ns/core#>,jp10=<http://jazz.net/xmlns/prod/jazz/process/1.0/>,oslc_config=<http://open-services.net/ns/config#>,dcterms=<http://purl.org/dc/terms/>',
-            oslc_prefix='dcterms=<http://purl.org/dc/terms/>',
-            # Note: Select "dcterms:identifier" field only in result
-            oslc_select=SELECT_ONE,
-        )
-        # -- todo: This test should be improved to know that it's really returning (only) the field I asked for...
-        self.assertGreater(len(query_result['Requirements']), 3, "Should be many results")
-        # self.assertNotIn('RequirementCollections', query_result, "No requirements collections should be returned")
-
-    def test_query_select_02(self):
-        """Locate dcterms:identifier=67120, select all fields ('*')"""
-        query_result = self.jazz.query(
-            # oslc_prefix='rdf=<http://www.w3.org/1999/02/22-rdf-syntax-ns#>,calm=<http://jazz.net/xmlns/prod/jazz/calm/1.0>,rm=<http://www.ibm.com/xmlns/rdm/rdf/>,oslc=<http://open-services.net/ns/core#>,jp10=<http://jazz.net/xmlns/prod/jazz/process/1.0/>,oslc_config=<http://open-services.net/ns/config#>,dcterms=<http://purl.org/dc/terms/>',
-            oslc_prefix='dcterms=<http://purl.org/dc/terms/>',
-            # oslc_prefix='dcterms=<http://purl.org/dc/terms/>,rdf=<http://www.w3.org/1999/02/22-rdf-syntax-ns#>,oslc=<http://open-services.net/ns/core#>,calm=<http://jazz.net/xmlns/prod/jazz/calm/1.0>,rm=<http://www.ibm.com/xmlns/rdm/rdf/>,oslc_config=<http://open-services.net/ns/config#,jp10=<http://jazz.net/xmlns/prod/jazz/process/1.0/>',
-            # Note: Return "all" the fields...
-            oslc_select=SELECT_ALL,
-            oslc_where=WHERE_ONE
-        )
-        # todo: Check that the correct fields are returned
-        self.assertEqual(query_result['result'], "Query Results: 1", "Should return only one record.")
-        self.assertEqual(len(query_result['Requirements']), 1, "Should be only one result")
-        self.assertNotIn('RequirementCollections', query_result, "No requirements collections should be returned")
-
-    def test_query_select_03(self):
-        """Locate dcterms:identifier=67120, select all fields ('*')"""
-        query_result = self.jazz.query(
-            oslc_prefix='dcterms=<http://purl.org/dc/terms/>',
-            # Note: Return "all" the fields...
             oslc_select='*',
-            oslc_where='dcterms:identifier=50161,dcterms:identifier=50162'
+            # This appears to work, but is very picky about formatting. :-(
+            oslc_where=WHERE_ONE
         )
         # todo: Check that the correct fields are returned
-        self.assertEqual(query_result['result'], "Query Results: 1", "Should return only one record.")
-        self.assertEqual(len(query_result['Requirements']), 1, "Should be only one result")
+        self.assertEqual("Query Results: 1", query_result['result'], "Should return two records.")
+        self.assertEqual(1, len(query_result['Requirements']), "Should be two result")
         self.assertNotIn('RequirementCollections', query_result, "No requirements collections should be returned")
 
+        description = query_result['query_root'].xpath(".//dcterms:description",  namespaces={'dcterms': "http://purl.org/dc/terms/"})
+        pass
+'''
 
 if __name__ == '__main__':
     unittest.main()
+    jazz.close()
     exit(0)
